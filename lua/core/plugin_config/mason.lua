32082dac082cf5c15150de11f60e2e127ab70da7
Jack Gage <jgage930@gmail.com>
2024-12-01 20:00:35 -0600

    mason

--- lua/core/plugin_config/mason.lua
+++ lua/core/plugin_config/mason.lua
@@ -1,4 +1,12 @@
 require("mason").setup()
 require("mason-lspconfig").setup {
-  ensure_installed = { "lua_ls", "rust_analyzer", "ruff" },
+  ensure_installed = { 
+    "lua_ls", 
+    "rust_analyzer", 
+    "ruff",
+    "gitlab_ci_ls",
+    "biome",
+    "bashls",
+    "nginx_language_server",
+  },
 }
