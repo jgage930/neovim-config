f355699bb4a8f8b5f3bc0ee3b1083380ed7b6cd3
Jack Gage <jgage930@gmail.com>
2024-12-01 19:10:01 -0600

    git signs

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -29,6 +29,7 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', 
     requires = {{ 'nvim-lua/plenary.nvim' }}
   }
+  use 'lewis6991/gitsigns.nvim'
   
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
